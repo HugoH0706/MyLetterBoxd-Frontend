6d14e1ffa79d54a3795115a83c9ebd4ac0a56b2e
HugoH0706 <hugohakkenberg@hotmail.nl>
2024-08-08 16:20:24 +0200

    finished homepage, to do entertainment page

--- MyLetterBoxd/src/app/app.routes.ts
+++ MyLetterBoxd/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { SignupComponent } from './features/signup/signup.component';
 import { AdminComponent } from './features/admin/admin.component';
 import { authGuard } from './auth/auth.guard';
 import { HomepageComponent } from './features/homepage/homepage.component';
+import { EntertainmentComponent } from './features/entertainment/entertainment.component';
 
 export const routes: Routes = [
     // {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: '', component: HomepageComponent},
     {path: 'login', component: LoginComponent},
     {path: 'signup', component: SignupComponent},
-    {path: 'admin', component: AdminComponent, canActivate: [authGuard]}
+    {path: 'admin', component: AdminComponent, canActivate: [authGuard]},
+    {path: 'entertainment', component: EntertainmentComponent}
 ];
